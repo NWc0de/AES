0ea30884711d1e940732200131e8067442be5f50
Spencer Little <Slittle57@gmail.com>
2019-08-29 12:54:50 -0700

    Add cli arg parser, implement method to read data into state

--- src/AES.java
+++ src/AES.java
@@ -1,50 +1,84 @@
 /**
  * Author: Spencer Little (mrlittle@uw.edu)
+ * Date: 08/28/18
  * An implementation of the AES (Rijndael) cipher
  */
 
-import java.util.Scanner;
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
 
 public class AES {
 
-    public static byte[][] stateArray; // The state (two dimensional array containing 128 bit block of input data)
-    public static String filePath; // Path to the file to be encrypted
+    public static int[][] stateArray; // The state (two dimensional array containing 128 bit block of input data)
+    public static Args cliArgs; // The cli object, facilitates parsing and specification of cli arguments
+    public static FileInputStream fileInput; // class Scanner to read data from file
 
     /*
-     * Performs the cipher operations
-     * Usage: -f path to file to be encrypted -o name of file for output -k key file  -m mode (CBC, tbd...)
+     * Orchestrates the cipher operations
+     * Usage: -f, -filepath: path to file to be encrypted -o, -output: name of file for output -k, -key: key file
      * @params command line arguments specifying cipher parameters
      */
-    public static void main(String[] args) {
-        stateArray = new byte[4][4];
-        // parse cli https://stackoverflow.com/questions/367706/how-do-i-parse-command-line-arguments-in-java
-        // http://commons.apache.org/proper/commons-cli/
+    public static void main(String[] argv) {
+        stateArray = new int[4][4];
+        fileInput = null;
+        // Parse the cli arguments
+        cliArgs = new Args();
+        try {
+            JCommander.newBuilder().addObject(cliArgs).build().parse(argv);
+        } catch (ParameterException prx) { // extraneous parameters raise exception
+            Args.showHelp();
+            System.exit(1);
+        }
+        cipher();
+    }
+
+    /*
+     * Performs one round of the cipher functions
+     * @params null, uses class var state to perform operations
+     * @return null, writes ciphertext to output array
+     */
+    private static void cipher() {
+        for (int z = 0; z < 14; z ++) {
+            readFileData();
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    System.out.println(stateArray[i][j]);
+                }
+            }
+        }
     }
 
     /*
      * Reads 128 bits from the specified filepath into the state array
      */
-    private void readFileData() {
-
-        Scanner fileInput = null;
-        try {
-            fileInput = new Scanner(new File(filePath));
-            int i = 0, j = 0;
-            while (fileInput.hasNextByte() && i < 4) {
-                stateArray[i][j] = fileInput.nextByte();;
-                j++;
-                if (j==4) {i++; j = 0;}
-            }
-        } catch (IOException iox) {
-            System.out.println("Error occurred during reading of file.");
-            iox.printStackTrace();
-        } finally {
-            if (fileInput != null) {
-                fileInput.close();
+    private static void readFileData() {
+        if (fileInput == null) {
+            try {
+                fileInput = new FileInputStream(cliArgs.filePath);
+            } catch (IOException iox) {
+                System.out.println("Error occurred while creating file stream.");
+                iox.printStackTrace();
+                System.exit(1);
             }
         }
+        // read data into state per NIST specification
+        //TODO: Padding?
+        try {
+            int i = 0, j = 0;
+            while (i < 4) {
+                stateArray[j][i] = fileInput.read();
+                j++;
+                if (j == 4) { i++; j = 0; }
+            }
+        } catch (IOException iox) {
+            System.out.println("Error occurred while reading file.");
+            iox.printStackTrace();
+            System.exit(1);
+        }
+        //TODO: Remember to close readers after final cipher loop
     }
 
 }
---
+++ src/Args.java
@@ -0,0 +1,28 @@
+/**
+ * Author: Spencer Little
+ * Date: 08/29/19
+ * A class to specify and parse cli arguments for the Main class
+ */
+
+import com.beust.jcommander.Parameter;
+
+public class Args {
+    @Parameter(names = { "-f", "-filepath" }, description = "Path to the file to be encrypted.")
+    public String filePath;
+
+    @Parameter(names = { "-o", "-output" }, description = "File name for the output.")
+    public String output;
+
+    @Parameter(names = { "-k", "-key" }, description = "Path to key file, or key in plaintext.")
+    public String key;
+
+    /*
+     * Displays a help message specifying accepted and required cli arguments
+     */
+    public static void showHelp() {
+        String help = "Usage: \njava AES -f|-filepath <path to file to be encrypted> " +
+                "-o|-output <filename for output> " +
+                "-k|key <path to keyfile or plaintext key>";
+        System.out.println(help);
+    }
+}
